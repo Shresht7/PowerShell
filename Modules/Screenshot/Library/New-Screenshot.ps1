4113dc929adaf1aa25b6d3bb0551efb5f7805e16
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-12-02 16:21:18 +0530

    Create `New-Screenshot` Function
    Takes a full screenshot and saves it to the Pictures\Screeshots folder

---
+++ Modules/Screenshot/Library/New-Screenshot.ps1
@@ -0,0 +1,27 @@
+<#
+.SYNOPSIS
+    Capture a screenshot
+.DESCRIPTION
+    Captures a screenshot and save it on disk
+.EXAMPLE
+    New-Screenshot
+    Captures a screenshot and saves it to the screenshot folder
+#>
+function New-Screenshot(
+    [ValidateScript({ Test-Path -Path $_ })]
+    [string] $Folder = "$HOME\Pictures\Screenshots"
+) {
+    Add-Type -AssemblyName System.Windows.Forms
+
+    $FilePath = Join-Path $Folder "Screenshot_$(Get-Date -Format FileDateTimeUniversal).png"
+
+    $VirtualScreen = [Windows.Forms.SystemInformation]::VirtualScreen
+    $ScreenshotBitmap = New-Object -TypeName Drawing.Bitmap $VirtualScreen.Width, $VirtualScreen.Height
+    $ScreenshotGraphics = [Drawing.Graphics]::FromImage($ScreenshotBitmap)
+
+    $ScreenshotGraphics.CopyFromScreen($VirtualScreen.Location, [Drawing.Point]::Empty, $VirtualScreen.Size)
+    $ScreenshotGraphics.Dispose()
+
+    $ScreenshotBitmap.Save($FilePath)
+    $ScreenshotBitmap.Dispose()
+}
