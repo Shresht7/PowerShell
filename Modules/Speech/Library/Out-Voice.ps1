2295d2c65bdc49e76939ccb4cff0a623d50ad02c
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-12-02 13:55:02 +0530

    Create `Out-Voice` Command

---
+++ Modules/Speech/Library/Out-Voice.ps1
@@ -0,0 +1,24 @@
+<#
+.SYNOPSIS
+    Speak out the given text
+.DESCRIPTION
+    Uses text-to-speech to speak out the provided text
+.EXAMPLE
+    Out-Voice -Text "Good Morning!"
+    Speaks "Good Morning!"
+.EXAMPLE
+    Get-Date | Out-Voice
+    Speaks the current date
+#>
+function Out-Voice(
+    # The text to speak out
+    [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+    [string] $Text,
+
+    # The ID of the voice to use
+    [string] $VoiceId = "*TTS_MS_EN-US_ZIRA_11.0"
+) {
+    $TTS = New-Object -ComObject SAPI.SPVoice
+    $TTS.Voice = $TTS.GetVoices() | Where-Object { $_.Id -like $VoiceId }
+    $null = $TTS.Speak($Text)
+}
