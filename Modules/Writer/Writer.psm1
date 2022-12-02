9786e0a33d84c61ba09ca15b15a305db63a7236d
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-12-02 12:57:51 +0530

    Update docstrings

--- Modules/Writer/Writer.psm1
+++ Modules/Writer/Writer.psm1
@@ -22,7 +22,7 @@ function Write-TypeWriter(
     [uint] $CPM = 350,
 
     # Speed of the typewriter (150ms by default)
-    [UInt32] $Speed = 150,
+    [uint] $Speed = 150,
 
     # The minimum time for a keystroke (default: 10ms)
     [uint] $MinSpeed = 10,
@@ -34,18 +34,30 @@ function Write-TypeWriter(
     [ValidatePattern("[0-9]+")]
     [uint] $PauseMultiplier = 3
 ) {
+
+    # Calculate the speed based on the given CPM (Characters-Per-Minute) if specified
     if ($CPM) {
         $Speed = (60 * 1000) / $CPM
     }
 
+    # Instantiate the Random Object
     $Random = New-Object -TypeName System.Random
+
     $Text -split '' | ForEach-Object {
+        # Determine the duration to pause for
         $PauseFor = $MinSpeed + $Random.Next($Speed)
+        # Apply pause multiplier if the character belongs to the $PauseAt set
         if ($PauseAt -contains $_) {
             $PauseFor *= $PauseMultiplier
         }
-        Start-Sleep -Milliseconds $PauseFor
+    
+        # Write the character to the screen
         Write-Host -NoNewline $_
+
+        # Pause for the duration
+        Start-Sleep -Milliseconds $PauseFor
     }
+
+    # Write empty newline at the end
     Write-NewLine
 }
