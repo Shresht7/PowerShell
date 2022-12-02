c8ce36af90779b7e4c9af75776dc7c2c480483ae
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-12-03 00:54:45 +0530

    Show more detailed output with `Git-GitRepo`

--- Modules/FSUtils/Library/Get-GitRepo.ps1
+++ Modules/FSUtils/Library/Get-GitRepo.ps1
@@ -14,7 +14,33 @@ function Get-GitRepo(
     [ValidateScript({ Test-Path -Path $_ })]
     [string] $Path = "."
 ) {
-    Get-ChildItem -Path $Path -Recurse -Directory -Hidden
-    | Where-Object { $_.Name -eq ".git" }
-    | ForEach-Object { $_.Parent }
+    begin {
+        $Res = [System.Collections.ArrayList]::new()
+    }
+
+    process {
+        Get-ChildItem -Path $Path -Recurse -Directory -Hidden
+        | Where-Object { $_.Name -eq ".git" }
+        | ForEach-Object {
+            $Folder = $_.Parent
+            $Branch = (git -C "$Folder" branch --show-current)
+            $LatestTagId = (git -C "$Folder" rev-list --tags --max-count=1)
+            if ($LatestTagId) {
+                $LatestTag = (git -C "$Folder" describe --tags $LatestTagId)
+            }
+            $Status = (git -C "$Folder" status --short)
+            $Status = if ($Status -like " M *") { "modified" } else { "clean" }
+            $null = $Res.Add([PSCustomObject]@{
+                    Name      = $Folder.BaseName
+                    Path      = $Folder
+                    Branch    = $Branch
+                    LatestTag = $LatestTag
+                    Status    = $Status
+                })
+        }
+    }
+
+    end {
+        $Res | Format-Table
+    }
 }
