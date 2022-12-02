755b15a6033926f5d338adfaeffd5f69c6dec4bd
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-12-02 17:13:54 +0530

    Create `Get-BatteryStatus` Utility Function

---
+++ Modules/System-Information/Library/Get-BatteryStatus.ps1
@@ -0,0 +1,7 @@
+function Get-BatteryStatus() {
+    Add-Type -AssemblyName System.Windows.Forms
+
+    $PowerStatus = [System.Windows.Forms.SystemInformation]::PowerStatus
+
+    $PowerStatus | Select-Object -Property *, @{Name = "BatteryLifePercentage"; Expression = { [int]($_.BatteryLifePercent * 100) } }, @{Name = "Time Remaining (Minutes)"; Expression = { $_.BatteryLifeRemaining / 60 } }
+}
