2555c5b7f4e957c19ec4bf883d36a68723f64a43
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-12-02 23:35:13 +0530

    Update docstrings

--- Modules/System-Information/Library/Get-BatteryStatus.ps1
+++ Modules/System-Information/Library/Get-BatteryStatus.ps1
@@ -1,7 +1,24 @@
+<#
+.SYNOPSIS
+    Get status of the battery
+.DESCRIPTION
+    Get status of the battery like battery percentage and time remaining
+.EXAMPLE
+    Get-BatteryStatus
+    Shows information about the battery
+#>
 function Get-BatteryStatus() {
     Add-Type -AssemblyName System.Windows.Forms
 
     $PowerStatus = [System.Windows.Forms.SystemInformation]::PowerStatus
 
-    $PowerStatus | Select-Object -Property *, @{Name = "BatteryLifePercentage"; Expression = { [int]($_.BatteryLifePercent * 100) } }, @{Name = "Time Remaining (Minutes)"; Expression = { $_.BatteryLifeRemaining / 60 } }
+    $PowerStatus | Select-Object -Property *,
+    @{
+        Name       = "BatteryLifePercentage"
+        Expression = { [int]($_.BatteryLifePercent * 100) }
+    },
+    @{
+        Name       = "Time Remaining (Minutes)"
+        Expression = { $_.BatteryLifeRemaining / 60 }
+    }
 }
--- Modules/System-Information/Library/Get-OperatingSystem.ps1
+++ Modules/System-Information/Library/Get-OperatingSystem.ps1
@@ -3,6 +3,9 @@
     Returns information about the operating system
 .DESCRIPTION
     Returns information about the operating system
+.EXAMPLE
+    Get-OperatingSystem
+    Shows information about the Operating System
 #>
 function Get-OperatingSystem() {
     $OS = Get-WmiObject -Class Win32_OperatingSystem
