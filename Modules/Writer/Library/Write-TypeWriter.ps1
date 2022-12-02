07862e83b6d4888d73c2f63055062c5dbb2d9be9
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-12-02 13:00:59 +0530

    Convert `Write-TypeWriter` to an advanced function

--- Modules/Writer/Library/Write-TypeWriter.ps1
+++ Modules/Writer/Library/Write-TypeWriter.ps1
@@ -8,51 +8,60 @@
 .DESCRIPTION
     Writes the given text like a typewriter would
 #>
-function Write-TypeWriter(
-    # The text to write
-    [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
-    [string] $Text,
+function Write-TypeWriter {
 
-    # Characters per minute
-    [uint] $CPM = 350,
-
-    # Speed of the typewriter (150ms by default)
-    [uint] $Speed = 150,
-
-    # The minimum time for a keystroke (default: 10ms)
-    [uint] $MinSpeed = 10,
-
-    # The list of characters to pause at
-    [string[]] $PauseAt = @(" ", "`n"),
-
-    # The the multiplier to apply when pausing
-    [ValidatePattern("[0-9]+")]
-    [uint] $PauseMultiplier = 3
-) {
-
-    # Calculate the speed based on the given CPM (Characters-Per-Minute) if specified
-    if ($CPM) {
-        $Speed = (60 * 1000) / $CPM
-    }
-
-    # Instantiate the Random Object
-    $Random = New-Object -TypeName System.Random
-
-    $Text -split '' | ForEach-Object {
-        # Determine the duration to pause for
-        $PauseFor = $MinSpeed + $Random.Next($Speed)
-        # Apply pause multiplier if the character belongs to the $PauseAt set
-        if ($PauseAt -contains $_) {
-            $PauseFor *= $PauseMultiplier
-        }
+    param (
+        # The text to write
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [string] $Text,
     
-        # Write the character to the screen
-        Write-Host -NoNewline $_
+        # Characters per minute
+        [uint] $CPM = 350,
+    
+        # Speed of the typewriter (150ms by default)
+        [uint] $Speed = 150,
+    
+        # The minimum time for a keystroke (default: 10ms)
+        [uint] $MinSpeed = 10,
+    
+        # The list of characters to pause at
+        [string[]] $PauseAt = @(" ", "`n"),
+    
+        # The the multiplier to apply when pausing
+        [ValidatePattern("[0-9]+")]
+        [uint] $PauseMultiplier = 3
+    )
 
-        # Pause for the duration
-        Start-Sleep -Milliseconds $PauseFor
+    begin {
+        # Instantiate the Random Object
+        $Random = New-Object -TypeName System.Random
+
+        # Calculate the speed based on the given CPM (Characters-Per-Minute) if specified
+        if ($CPM) {
+            $Speed = (60 * 1000) / $CPM
+        }
     }
 
-    # Write empty newline at the end
-    Write-NewLine
+    process {    
+        $Text -split '' | ForEach-Object {
+            # Determine the duration to pause for
+            $PauseFor = $MinSpeed + $Random.Next($Speed)
+            # Apply pause multiplier if the character belongs to the $PauseAt set
+            if ($PauseAt -contains $_) {
+                $PauseFor *= $PauseMultiplier
+            }
+        
+            # Write the character to the screen
+            Write-Host -NoNewline $_
+    
+            # Pause for the duration
+            Start-Sleep -Milliseconds $PauseFor
+        }
+    }
+
+    end {
+        # Write empty newline at the end
+        Write-NewLine
+    }
+
 }
