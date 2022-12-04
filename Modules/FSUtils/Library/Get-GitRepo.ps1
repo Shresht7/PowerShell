c9081c0081a65ea0b3bb042f4c8a40c8316e229a
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-12-04 21:29:42 +0530

    Update documentation

--- Modules/FSUtils/Library/Get-GitRepo.ps1
+++ Modules/FSUtils/Library/Get-GitRepo.ps1
@@ -1,36 +1,52 @@
 <#
 .SYNOPSIS
-    Returns a list of git repositories
+    Get information about Git repositories in a specified directory
 .DESCRIPTION
-    Returns a list of git repositories in the given path looking recursively.
+    This cmdlet searches the specified directory and its subdirectories for Git repositories, and then
+    returns information about each repository it finds. This includes the repository's name, path, current branch,
+    latest tag, and status (modified or clean)
+.OUTPUTS
+    System.Object
+        An object that contains information about each Git repository that was found.
 .EXAMPLE
     Get-GitRepo
-    Returns a list of git repositories in the current directory
+    Returns information about all Git repositories in the current directory and its subdirectories.
 .EXAMPLE
-    Get-Repo -Path ".\Some\Other\Path"
-    Returns a list of git repositories in some other path
+    Get-GitRepo -Path "C:\Repos"
+    Returns information about all Git repositories in the specified directory and its subdirectories.
+.NOTES
+    This function requires the `git` command-line utility to be installed and available on the system.
 #>
 function Get-GitRepo(
+    # The path to the directory to search for Git repositories. Defaults to the current directory.
     [ValidateScript({ Test-Path -Path $_ })]
     [string] $Path = "."
 ) {
     begin {
-        $Res = [System.Collections.ArrayList]::new()
+        $Results = [System.Collections.ArrayList]::new()
     }
 
     process {
+        # Find `.git` directories
         Get-ChildItem -Path $Path -Recurse -Directory -Hidden
         | Where-Object { $_.Name -eq ".git" }
         | ForEach-Object {
+            # Select the parent folder
             $Folder = $_.Parent
+
+            # Get the current branch
             $Branch = (git -C "$Folder" branch --show-current)
+            # Get the latest tag
             $LatestTagId = (git -C "$Folder" rev-list --tags --max-count=1)
             if ($LatestTagId) {
                 $LatestTag = (git -C "$Folder" describe --tags $LatestTagId)
             }
+            # Get the current status of the repository
             $Status = (git -C "$Folder" status --short)
             $Status = if ($Status -like " M *") { "modified" } else { "clean" }
-            $null = $Res.Add([PSCustomObject]@{
+
+            # Store the results
+            $null = $Results.Add([PSCustomObject]@{
                     Name      = $Folder.BaseName
                     Path      = $Folder
                     Branch    = $Branch
@@ -41,6 +57,6 @@ function Get-GitRepo(
     }
 
     end {
-        $Res | Format-Table
+        $Results | Format-Table
     }
 }
--- Modules/FSUtils/Library/Get-SpecialFolders.ps1
+++ Modules/FSUtils/Library/Get-SpecialFolders.ps1
@@ -1,11 +1,17 @@
 <#
 .SYNOPSIS
-    Returns a list of special folders and their paths
+    Get the paths of special folders on the system
 .DESCRIPTION
-    Returns a list of special folders and their paths
+    The `Get-SpecialFolders` function returns a list of objects that contain the names and paths of special folders
+    on the system. Special folders are predefined system folders that are used to store specific types of files,
+    such as the Desktop or the Start Menu.
+.OUTPUTS
+    System.Object
+        An object that contains the name and path of each special folder on the system
 .EXAMPLE
     Get-SpecialFolders
-    Returns a list of special folders and their paths
+        Returns a list of objects that contain the names and paths of special folders on the system
+
 #>
 function Get-SpecialFolders() {
     $SpecialFolders = [System.Enum]::GetNames('System.Environment+SpecialFolder')
