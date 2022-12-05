296950b00742dd4a17cfa12f41e86dc08c4b9d1b
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-12-05 23:56:23 +0530

    Create `Start-App` Utility

---
+++ Modules/Utilities/Library/Start-App.ps1
@@ -0,0 +1,21 @@
+<#
+.SYNOPSIS
+    Starts an application
+.DESCRIPTION
+    Start an application from the shell:AppsFolder
+.EXAMPLE
+    Start-App -Name Clock
+    Start the Windows Clock application
+.EXAMPLE
+    Get-StartApps | Invoke-Fzf | Start-App
+    Interactively select the app using fzf and start it
+#>
+function Start-App(
+    # Name of the Application. (Use `Get-StartApps` to get a list of apps)
+    [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+    [string]$Name
+) {
+    $App = Get-StartApps | Where-Object { $_.Name -eq $Name }
+    if (-Not $App) { return }
+    Start-Process "explorer.exe" -ArgumentList "shell:AppsFolder\$(($App).AppId)"
+}
