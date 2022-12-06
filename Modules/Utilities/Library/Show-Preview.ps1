733395254f18cc100e6a3c78397c274d700689ad
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-12-06 21:16:47 +0530

    Create `Show-Preview` Function

---
+++ Modules/Utilities/Library/Show-Preview.ps1
@@ -0,0 +1,12 @@
+# -------------
+# Show-Previews
+# -------------
+
+function Show-Previews() {
+    fzf --preview 'bat --style=numbers --color=always --line-range :500 {}' --preview-window=right:60%
+}
+
+Set-Alias preview Show-Preview
+
+Export-ModuleMember -Function Show-Preview
+Export-ModuleMember -Alias preview
