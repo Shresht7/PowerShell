6b2a0b7cddb35b2b4454dd33f40ba9fd03f9f12b
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-12-06 20:31:48 +0530

    Create Auto-Complete Scaffolding

--- Modules/AutoComplete/AutoComplete.psm1
+++ Modules/AutoComplete/AutoComplete.psm1
@@ -1,3 +1,8 @@
+# Import Class
+Get-ChildItem -Path "$PSScriptRoot\Class" -Filter "*.ps1" | ForEach-Object {
+    . $_.FullName -Force -Verbose
+}
+
 # Import Library
 Get-ChildItem -Path "$PSScriptRoot\Library" -Filter "*.ps1" | ForEach-Object {
     . $_.FullName -Force -Verbose
