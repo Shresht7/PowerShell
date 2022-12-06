34c9ad4312d349c92ff96056622420aa7a091d8a
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-12-06 21:12:11 +0530

    Create `Backup` Module

---
+++ Modules/Backup/Backup.ps1
@@ -0,0 +1,6 @@
+$Script:BACKUP_PATH = "$HOME\Archives\Backups"
+
+# Import Library
+Get-ChildItem -Path "$PSScriptRoot\Library" -Filter "*.ps1" | ForEach-Object {
+    . $_.FullName -Force -Verbose
+}
