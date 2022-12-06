b7fc3b37a0f2bf2134aafb80de65fa77d07894b4
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-12-07 02:14:48 +0530

    Fix typo

--- Modules/Utilities/Library/Show-Preview.ps1
+++ Modules/Utilities/Library/Show-Preview.ps1
@@ -2,7 +2,7 @@
 # Show-Previews
 # -------------
 
-function Show-Previews() {
+function Show-Preview() {
     fzf --preview 'bat --style=numbers --color=always --line-range :500 {}' --preview-window=right:60%
 }
 
