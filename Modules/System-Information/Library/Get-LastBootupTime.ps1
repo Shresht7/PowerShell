488a8499584d30c6a4cab26b8d026f755698318a
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-12-08 21:17:25 +0530

    Return `Duration: TimeSpan` in `Get-LastBootUpTime`

--- Modules/System-Information/Library/Get-LastBootupTime.ps1
+++ Modules/System-Information/Library/Get-LastBootupTime.ps1
@@ -7,12 +7,20 @@
     Get-LastBootUpTime
 #>
 function Get-LastBootUpTime(
-    [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+    [Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName)]
+    [Alias("Name", "HostName", "Computer", "SystemName")]
     [string] $ComputerName = $Env:COMPUTERNAME
 ) {
     $LastBootUpTime = (Get-CimInstance -ClassName Win32_OperatingSystem | Select-Object -Property LastBootUpTime).LastBootUpTime
+    $Duration = New-TimeSpan -Start $LastBootUpTime -End (Get-Date)
     [PSCustomObject]@{
         ComputerName   = $ComputerName
         LastBootUpTime = $LastBootUpTime
+        Duration       = $Duration
+        Days           = $Duration.Days
+        Hours          = $Duration.Hours
+        Minutes        = $Duration.Minutes
+        Seconds        = $Duration.Seconds
+        Milliseconds   = $Duration.Milliseconds
     }
 }
