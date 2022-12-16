bbced81f0c699d7fd9851600fcc7667f18658752
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-12-17 01:12:10 +0530

    Refactor `Set-FuzzyLocation.ps1`

--- Modules/FSUtils/Library/Set-FuzzyLocation.ps1
+++ Modules/FSUtils/Library/Set-FuzzyLocation.ps1
@@ -2,8 +2,32 @@
 # Set-FuzzyLocation
 # -----------------
 
-function Set-FuzzyLocation() {
-    fd --type directory | fzf | Set-Location
+<#
+.SYNOPSIS
+    Set the current location to a directory selected by fzf.
+.DESCRIPTION
+    Set the current location to a directory selected by fzf.
+.EXAMPLE
+    Set-FuzzyLocation
+.EXAMPLE
+    Set-FuzzyLocation -Path Module
+#>
+function Set-FuzzyLocation(
+    # The path to set as the initial query for fzf
+    [Parameter(Position = 0, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [Alias('FullName', 'Name')]
+    [string] $Path
+) {
+    # Set Fzf Options
+    $FzfOptions = @{
+        Query   = $Path
+        Preview = "pwsh -NoProfile -Command Get-ChildItem -Path {}"
+    }
+
+    # Set the current location to the selected directory
+    Get-ChildItem -Directory
+    | Invoke-Fzf @FzfOptions 
+    | Set-Location
 }
 
 Set-Alias cdf Set-FuzzyLocation
