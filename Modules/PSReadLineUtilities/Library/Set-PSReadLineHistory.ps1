c5c945a47f4de12e37e46a57e54b36b9e55f4102
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-01-27 16:46:16 +0530

    Refactor to handle strings only

--- Modules/PSReadLineUtilities/Library/Set-PSReadLineHistory.ps1
+++ Modules/PSReadLineUtilities/Library/Set-PSReadLineHistory.ps1
@@ -5,16 +5,18 @@
     Sets (overwrites) the contents of the PSReadLine history file. Useful after performing
     some manipulation on the existing history (using Get-PSReadLineHistory)
 .EXAMPLE
-    Set-PSReadLineHistory ($Content)
+    Set-PSReadLineHistory -Content ($Content)
     Updates the PSReadLineHistory Contents with $Contents
+.EXAMPLE
+    Set-PSReadLineHistory -Content (Get-PSReadLineHistory | Where-Object { $_ -notmatch "Get-Service" })
+    Removes all instances of the "Get-Service" command from the PSReadLine history.
 #>
 function Set-PSReadLineHistory(
     # The new contents of the PSReadLine history file
     [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
-    [string[]]$Content
+    [string] $Content
 ) {
     $Path = Get-PSReadLineHistoryPath
-    $Temp = "$Path.temp"
-    $Content | Out-File $Temp
+    $Content | Out-File -FilePath "$Path.temp"
     Move-Item -Path $Temp -Destination $Path -Force
 }
