98f47c0e172e24e77c1d7ab98cb747cf61592b7a
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-01-28 02:42:04 +0530

    Refactor `Search-FullText`

--- Modules/Utilities/Library/Search-FullText.ps1
+++ Modules/Utilities/Library/Search-FullText.ps1
@@ -6,17 +6,26 @@
 
 <#
 .SYNOPSIS
-Performs full-text search
+    Performs full-text search
 .DESCRIPTION
-Uses ripgrep (rg) and fuzzy-finder (fzf) to perform an interactive full-text search and shows the preview using bat
+    Uses ripgrep (rg) and fuzzy-finder (fzf and PSFzf) to perform an interactive full-text search and shows the preview using bat
 .PARAMETER text
-The text or regular expression to search for
+    The text or regular expression to search for
 .EXAMPLE
-Search-FullText TODO:		# Searches for all file containing TODO:
+    Search-FullText TODO:
+    Searches for all file containing TODO:
 #>
-function Search-FullText($text) {
-    # rg $text --line-number | fzf --delimiter=":" --preview 'bat --style=numbers --color=always --highlight-line {2} --line-range {2}: {1}'
-    Invoke-PsFzfRipgrep $text
+function Search-FullText(
+    # The string to search for
+    [Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName)]
+    [Alias('InputObject', 'Search', 'Query', 'Text')]
+    [string] $SearchString,
+
+    # Do not open the file in the editor
+    [switch] $NoEditor
+) {
+    # rg $SearchString --line-number | fzf --delimiter=":" --preview 'bat --style=numbers --color=always --highlight-line {2} --line-range {2}: {1}'
+    Invoke-PsFzfRipgrep -SearchString $SearchString -NoEditor:$NoEditor
 }
 
 Set-Alias fts Search-FullText
