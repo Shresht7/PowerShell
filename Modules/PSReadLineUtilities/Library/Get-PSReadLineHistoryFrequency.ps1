899235fd6fbd93f126d355292610f5712651a44c
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-01-27 19:39:09 +0530

    Add comments

--- Modules/PSReadLineUtilities/Library/Get-PSReadLineHistoryFrequency.ps1
+++ Modules/PSReadLineUtilities/Library/Get-PSReadLineHistoryFrequency.ps1
@@ -8,15 +8,21 @@
     Returns a hash-table containing commands from the PSReadLine history and their usage frequency
 #>
 function Get-PSReadLineHistoryFrequency {
+    # Variable to track the frequency of each command
     $frequency = @{}
+
+    # Iterate over the history
     foreach ($line in Get-PSReadLineHistory) {
+        # If the command is already in the hash-table, increment the count
         if (!$frequency[$line]) {
             $frequency.Add($line, 1)
         }
+        # Otherwise, add the command to the hash-table
         else {
             $frequency[$line]++
         }
     }
-    $sorted = $frequency.GetEnumerator() | Sort-Object -Property Value
-    Write-Output $sorted
+
+    # Return the hash-table
+    $frequency.GetEnumerator() | Sort-Object -Property Value
 }
