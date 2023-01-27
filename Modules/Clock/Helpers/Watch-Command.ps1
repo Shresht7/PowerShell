cb3b8c73e2127630f17d0d8fc2a3d1eee77245d8
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-01-27 15:25:41 +0530

    Refactor docstrings

--- Modules/Clock/Helpers/Watch-Command.ps1
+++ Modules/Clock/Helpers/Watch-Command.ps1
@@ -5,12 +5,6 @@
     Periodically execute a script-block on a given time interval.
     This allows to see the program output change over time. By default the interval is set to 1 second.
     This script-block runs until explicitly stopped.
-.PARAMETER $ScriptBlock
-    The Script-Block to execute repeatedly
-.PARAMETER $Interval
-    The update interval timespan
-.PARAMETER $ClearHost
-    Switch to clear the host screen on each tick
 .EXAMPLE
     Watch-Command { Get-Date }
     Will execute `Get-Date` every second
@@ -19,9 +13,14 @@
     Will execute `Get-Date` every 60 seconds
 #>
 function Watch-Command(
+    # The Script-Block to execute repeatedly
     [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
     [scriptblock] $ScriptBlock,
+
+    # The update interval timespan (default 1 second)
     [int32] $Interval = 1,
+
+    # Switch to clear the host screen on each tick
     [switch] $ClearHost
 ) {
     while ($True) {
