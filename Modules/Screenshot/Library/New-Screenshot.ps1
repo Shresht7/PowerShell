9d00b964bd4bbd47fdc09a6e8f27e799f75cc8cf
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-01-29 15:02:21 +0530

    Minor Refactor

--- Modules/Screenshot/Library/New-Screenshot.ps1
+++ Modules/Screenshot/Library/New-Screenshot.ps1
@@ -11,26 +11,37 @@
     Captures a screenshot and opens it with default application
 #>
 function New-Screenshot(
+    # The path to the screenshot folder
+    [Alias('Path', 'Destination', 'DestinationFolder', 'Target')]
     [ValidateScript({ Test-Path -Path $_ })]
     [string] $Folder = $Script:ScreenshotFolder,
+
+    # The screenshot's filename
+    [Alias('Item', 'File', 'Screenshot')]
+    [string] $Name,
 
     # Open the screenshot
     [switch] $Open
 ) {
+    # Import Windows Forms
     Add-Type -AssemblyName System.Windows.Forms
 
-    $FilePath = Join-Path $Folder "Screenshot_$(Get-Date -Format FileDateTimeUniversal).png"
+    # Determine FilePath
+    $FileName = $Name -or "Screenshot_$(Get-Date -Format FileDateTimeUniversal).png"
+    $FilePath = Join-Path -Path $Folder -ChildPath $FileName
 
+    # Capture Graphics
     $VirtualScreen = [Windows.Forms.SystemInformation]::VirtualScreen
     $ScreenshotBitmap = New-Object -TypeName Drawing.Bitmap $VirtualScreen.Width, $VirtualScreen.Height
     $ScreenshotGraphics = [Drawing.Graphics]::FromImage($ScreenshotBitmap)
-
     $ScreenshotGraphics.CopyFromScreen($VirtualScreen.Location, [Drawing.Point]::Empty, $VirtualScreen.Size)
     $ScreenshotGraphics.Dispose()
 
+    # Save to Disk
     $ScreenshotBitmap.Save($FilePath)
     $ScreenshotBitmap.Dispose()
 
+    # If the `-Open` switch is passed, open the screenshot
     if ($Open) {
         Invoke-Item -Path $FilePath
     }
