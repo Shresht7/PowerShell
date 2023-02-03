ca8dfc811ab93e301330a296425b8b93977928ff
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-02-03 11:47:20 +0530

    Create `Get-NpmScript` Function

---
+++ Modules/NodeUtils/Library/Get-NpmScript.ps1
@@ -0,0 +1,37 @@
+<#
+.SYNOPSIS
+    Get the list of npm scripts from a package.json file
+.DESCRIPTION
+    Get the list of npm scripts from a package.json file
+.EXAMPLE
+    Get-NpmScript
+    Get the list of npm scripts from the package.json file in the current directory
+.EXAMPLE
+    Get-NpmScript -Path "C:\Projects\MyProject"
+    Get the list of npm scripts from the package.json file in the given directory
+.EXAMPLE
+    Get-NpmScript -Path "C:\Projects\MyProject\package.json"
+    Get the list of npm scripts from the package.json file in the given path
+#>
+function Get-NpmScript(
+    # Path to the package.json file
+    [Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName, Position = 0)]
+    [ValidateScript({ Test-Path -Path $_ })]
+    [string]$Path = $PWD.Path
+) {
+
+    # Get the package.json contents
+    $Package = Get-PackageJson -Path $Path
+
+    # Exit if the package is not found
+    if (-Not $Package) { return }
+
+    # Get the npm scripts from the package.json file
+    $Scripts = $Package.scripts
+    | Get-Member -MemberType NoteProperty
+    | Select-Object -Property Name, @{Name = "Script"; Expression = { $Package.scripts.($_.Name) } }
+
+    # Return the list of scripts
+    return $Scripts
+    
+}
