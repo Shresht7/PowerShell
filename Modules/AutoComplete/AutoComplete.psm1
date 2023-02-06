c0214ec10ec2e80640826fc30822c39cc8c792cd
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-02-06 16:48:09 +0530

    Rename file to `Register-CommandCompleter`

--- Modules/AutoComplete/AutoComplete.psm1
+++ Modules/AutoComplete/AutoComplete.psm1
@@ -1,11 +1,9 @@
 # Import Class
 Get-ChildItem -Path "$PSScriptRoot\Class" -Filter "*.ps1" | ForEach-Object {
     . $_.FullName -Force -Verbose
-    Export-ModuleMember -Function $_.BaseName
 }
 
 # Import Library
 Get-ChildItem -Path "$PSScriptRoot\Library" -Filter "*.ps1" | ForEach-Object {
     . $_.FullName -Force -Verbose
-    Export-ModuleMember -Function $_.BaseName
 }
