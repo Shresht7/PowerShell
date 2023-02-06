9cc976d8f872b70563c731640e446be28badbae2
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-02-06 16:22:31 +0530

    `Helpers` and `Commands` no longer exist

--- Modules/AutoComplete/AutoComplete.psm1
+++ Modules/AutoComplete/AutoComplete.psm1
@@ -4,18 +4,8 @@ Get-ChildItem -Path "$PSScriptRoot\Class" -Filter "*.ps1" | ForEach-Object {
     Export-ModuleMember -Function $_.BaseName
 }
 
-# Import Helpers
-Get-ChildItem -Path "$PSScriptRoot\Helpers" -Filter "*.ps1" | ForEach-Object {
-    . $_.FullName -Force -Verbose
-}
-
 # Import Library
 Get-ChildItem -Path "$PSScriptRoot\Library" -Filter "*.ps1" | ForEach-Object {
     . $_.FullName -Force -Verbose
     Export-ModuleMember -Function $_.BaseName
-}
-
-# Import Commands
-Get-ChildItem -Path "$PSScriptRoot\Commands" -Filter "*.ps1" | ForEach-Object {
-    . $_.FullName -Force -Verbose
 }
