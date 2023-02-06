b518c6f715883e940d9158e5c8c42dd9e1d027e4
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-02-06 15:58:46 +0530

    Add Styles to `Write-Separator`

--- Modules/Writer/Library/Write-Separator.ps1
+++ Modules/Writer/Library/Write-Separator.ps1
@@ -7,22 +7,45 @@
     Write-Separator
 .EXAMPLE
     Write-Separator -Character "*"
+    ******************************************************
 .EXAMPLE
     Write-Separator -Character '-'
+    ------------------------------------------------------
 .EXAMPLE
     Write-Separator -Character '=' -Length 80
+    ======================================================================
+.EXAMPLE
+    Write-Separator -Color $PSStyle.Foreground.Red
+    Create a red colored separator
+.EXAMPLE
+    Write-Separator -Color $PSStyle.Foreground.Red, $PSStyle.Bold
+    Create a separator that is red in color and bold
 #>
 function Write-Separator(
-    # The character to use for the separator
+    # The character to use for the separator. (Defaults to "-")
     [Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName, Position = 0)]
     [Alias('Symbol', 'Char')]
     [string] $Character = '-',
 
-    # The number of times to repeat the character
+    # The number of times to repeat the character. (Defaults to the WindowSize.Width)
     [Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName, Position = 1)]
     [Alias('Length')]
-    [int] $Width = $Host.UI.RawUI.WindowSize.Width
+    [int] $Width = $Host.UI.RawUI.WindowSize.Width,
+
+    # The ansi color/style of the separator
+    [Parameter(Position = 2)]
+    [Alias('Color')]
+    [string[]] $Style
 ) {
-    # Draw the separator
-    Write-Host -Object ($Character * $Width)
+    # Create the separator
+    $Separator = ($Character * $Width)
+
+    # If a color/style has been passed, wrap it around the separator text
+    if ($Style -and $Style.Length -gt 0) {
+        $Style = $Style -join ""
+        $Separator = $Style + ($Character * ($Width - 3)) + $PSStyle.Reset
+    }
+
+    # Draw the separator to the console
+    Write-Host -Object $Separator
 }
