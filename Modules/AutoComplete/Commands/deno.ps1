2514f1bb509242b32f2680f0ec78d3b1f6c2de82
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-02-06 16:16:29 +0530

    Reduce `AutoComplete` to an auxiliary module
    Removed all autocomplete setup for external commands. Its probably better to do this in their respective modules as and when they're needed.

--- Modules/AutoComplete/Commands/deno.ps1
+++
@@ -1,7 +0,0 @@
-# ----
-# Deno
-# ----
-
-if (-Not (Find-Path deno)) { return }
-
-Invoke-Expression -Command $(deno completions powershell | Out-String)
--- Modules/AutoComplete/Commands/gh.ps1
+++
@@ -1,7 +0,0 @@
-# ----------
-# GitHub CLI
-# ----------
-
-if (-Not (Find-Path gh)) { return }
-
-Invoke-Expression -Command $(gh completion -s powershell | Out-String)
