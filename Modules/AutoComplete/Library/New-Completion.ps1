927996123f32b9d68b3a93413729c8604bdf4b17
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-02-06 22:23:33 +0530

    Refactor: Improve code

--- Modules/AutoComplete/Library/New-Completion.ps1
+++ Modules/AutoComplete/Library/New-Completion.ps1
@@ -5,10 +5,25 @@
     Instantiate a new Completion object
 #>
 function New-Completion(
+    # The name of the command
+    [Parameter(Mandatory = $True, Position = 0)]
+    [Alias('Command', 'CommandName')]
     [string] $Name,
+
+    # The tooltip to display when the command is selected
+    [Parameter(Position = 1)]
+    [Alias('Description', 'Help', 'HelpText', 'HelpMessage', 'Message')]
     [string] $Tooltip,
+
+    # The next set of completions
+    [Parameter(Position = 2)]
+    [Alias('Next', 'NextCompletions', 'SubCompletions')]
     [Completion[]] $Completions,
-    [scriptblock] $Script
+
+    # The script block to execute to get the next set of completions
+    [Parameter(Position = 3)]
+    [Alias('Script', 'ScriptBlock')]
+    [scriptblock] $CompletionsScript
 ) {
 
     # If no tooltip was passed in, use the name
@@ -18,8 +33,8 @@ function New-Completion(
     if (($null -eq $Completions) -or ($Completions.Length -eq 0)) { $Completions = @() }
 
     # If no script block is provided, use $null
-    if ($null -eq $Script) { $Script = $null }
+    if ($null -eq $CompletionsScript) { $CompletionsScript = $null }
 
     # Return the new Completion object
-    return [Completion]::new($Name, $Tooltip, $Completions, $Script)
+    return [Completion]::new($Name, $Tooltip, $Completions, $CompletionsScript)
 }
