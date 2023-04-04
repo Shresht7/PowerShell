4c4e50bc30ee35e2e5915ef961abbc0f2a8d2c22
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-04-04 11:34:55 +0530

    Add `Latest` switch to retrieve the latest screenshot

--- Modules/Screenshot/Library/Get-Screenshot.ps1
+++ Modules/Screenshot/Library/Get-Screenshot.ps1
@@ -12,6 +12,9 @@
 .EXAMPLE
     Get-Screenshot | Sort-Object -Property LastWriteTime | Select-Object -First 1 | Invoke-Item
     Opens the latest screenshot with the default application
+.EXAMPLE
+    Get-Screenshot -Latest | Invoke-Item
+    Opens the latest screenshot with the default application
 #>
 function Get-Screenshot(
     # The folder where the screenshots are stored
@@ -20,7 +23,20 @@ function Get-Screenshot(
     [string] $Path = $Script:ScreenshotFolder,
 
     # Filter the list of screenshots
-    [string] $Filter
+    [string] $Filter,
+
+    # Switch to get the latest screenshot
+    [Parameter(ParameterSetName = "Latest")]
+    [Alias("Newest", "MostRecent")]
+    [switch] $Latest
 ) {
+
+    # If the Latest parameter is specified, return the most recent screenshot
+    if ($PSCmdlet.ParameterSetName -eq "Latest") {
+        # Return the most recent screenshot
+        return Get-ChildItem -Path $Path -Filter:$Filter -Recurse | Sort-Object -Property LastWriteTime | Select-Object -First 1
+    }
+
+    # Return the list of screenshots
     Get-ChildItem -Path $Path -Filter:$Filter -Recurse
 }
