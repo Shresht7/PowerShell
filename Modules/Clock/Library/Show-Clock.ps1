c6c3aee7b5bf8aac0e3a760162c0f89694104273
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-06-16 19:52:34 +0530

    Update docstrings

--- Modules/Clock/Library/Show-Clock.ps1
+++ Modules/Clock/Library/Show-Clock.ps1
@@ -1,10 +1,11 @@
 <#
 .SYNOPSIS
-    Shows the clock
+    Shows the clock displaying the current time and date in the console.
 .DESCRIPTION
     Show the time and date. Refreshes every 1 second.
 .EXAMPLE
     Show-Clock
+    Displays the current time and date in the console.
 #>
 function Show-Clock() {
     Clear-Host
