58f686fc23cbc9b7ce1c2564af86d010b086abc5
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-06-16 15:49:57 +0530

    Update docstrings

--- Modules/Backup/Library/Get-Backup.ps1
+++ Modules/Backup/Library/Get-Backup.ps1
@@ -4,15 +4,19 @@
 
 <#
 .SYNOPSIS
-Get the list of backups
+Get-Backup: Get the list of backups.
 .DESCRIPTION
-Returns the directory listing of the backup directory
+Returns the directory listing of the backup directory.
+.PARAMETER Filter
+Filter the list of backup items by name.
+.PARAMETER BackupPath
+Specify the location of the backups directory.
 .EXAMPLE
 Get-Backup
-Returns a list of all backups
+Returns a list of all backups.
 .EXAMPLE
 Get-Backup -Filter '*Console*'
-Returns a list of backups that match the given criteria
+Returns a list of backups that match the given criteria.
 #>
 function Get-Backup(
     # Filter the list of backup items
