19a9aec5b08077585e4c84915de5970634361e98
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-06-17 21:22:57 +0530

    Refactor `Get-BrokenSymlink`

--- Modules/FSUtils/Library/Get-BrokenSymlink.ps1
+++ Modules/FSUtils/Library/Get-BrokenSymlink.ps1
@@ -3,6 +3,10 @@
     Gets the broken links
 .DESCRIPTION
     Gets the broken links (that do not point to a valid location) in the given directory.
+.PARAMETER Path
+    The path to search for broken symlinks. If not specified, the current directory is used.
+.PARAMETER Recurse
+    Specifies whether to search for broken symlinks recursively within subdirectories. By default, only the specified path is searched.
 .EXAMPLE
     Get-BrokenSymlinks
     Returns a list of broken symlinks
@@ -11,10 +15,12 @@
     Removes all broken symlinks asking as you go
 #>
 function Get-BrokenSymlink(
-    # The path to look inside of
+    # The path to search for broken symlinks. If not specified, the current directory is used.
     [Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName)]
-    [string] $Path = ".",
-    # Search recursively
+    [ValidateScript({ Test-Path $_ -PathType 'Container' })]
+    [string]$Path = ".",
+
+    # Specifies whether to search for broken symlinks recursively within subdirectories. By default, only the specified path is searched.
     [switch]$Recurse
 ) {
     # Recursively get all children and filter out links. Then filter the links again to those who do not have a valid link target
