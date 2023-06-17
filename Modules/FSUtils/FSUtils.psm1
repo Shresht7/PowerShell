4d95872acbfc2db761c37158112cc97baaa1097f
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-06-17 20:30:50 +0530

    Export functions recursively

--- Modules/FSUtils/FSUtils.psm1
+++ Modules/FSUtils/FSUtils.psm1
@@ -1,4 +1,4 @@
 # Import Library
-Get-ChildItem -Path "$PSScriptRoot\Library" -Filter "*.ps1" | ForEach-Object {
+Get-ChildItem -Path "$PSScriptRoot\Library" -Filter "*.ps1" -Recurse | ForEach-Object {
     . $_.FullName -Force -Verbose
 }
