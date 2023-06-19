5928f60e43c972e33616b7ca18fc33f872000b0b
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-06-19 18:04:03 +0530

    Refactor `Get-RandomString` into `New-RandomString`

--- Modules/Writer/Library/New-RandomString.ps1
+++ Modules/Writer/Library/New-RandomString.ps1
@@ -1,22 +1,24 @@
 <#
 .SYNOPSIS
-    Generates a random string
+    Generates a random string.
 .DESCRIPTION
     Generates a random string. The string is generated using the characters in the $Characters parameter.
 .EXAMPLE
-    Get-RandomString -Length 10
+    New-RandomString -Length 10
     Generates a random string of 10 characters
 .EXAMPLE
-    Get-RandomString -Length 10 -Characters "abc"
+    New-RandomString -Length 10 -Characters "abc"
     Generates a random string of 10 characters using only the characters "a", "b" and "c"
 #>
-function Get-RandomString(
+function New-RandomString(
     # The length of the random string
     [Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName)]
-    [int32] $Length = 10,
+    [ValidateRange()]
+    [UInt32] $Length = 10,
 
     # The characters to use for the random string
     [Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName)]
+    [ValidateNotNullOrEmpty()]
     [string] $Characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
 ) {
     $Random = New-Object System.Random
