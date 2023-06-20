a6e6d44f05885e79915dcbde44009773e9e86327
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-06-20 22:05:41 +0530

    Point to the correct `searchEngines.json` file

--- Modules/Search-Web/Search-Web.psm1
+++ Modules/Search-Web/Search-Web.psm1
@@ -1,35 +1,10 @@
-$SearchEngines = Get-Content -Path "$PSScriptRoot\searchEngines.json" | ConvertFrom-Json
+# Search-Engines JSON file
+$Script:SearchEnginesJsonFile = "$HOME\Data\searchEngines.json"
 
-<#
-.SYNOPSIS
-    Search the web
-.DESCRIPTION
-    Launches the default web-browser to do a web-search using a search-engine
-.EXAMPLE
-    Search-Web 'PowerShell Documentation'
-    Searches the web for 'PowerShell Documentation' using the default search engine
-.EXAMPLE
-    Search-Web 'Microsoft PowerToys' bing
-    Searches the web for 'Microsoft PowerToys' using the 'bing' search engine
-.EXAMPLE
-    Search-Web -engine github -query 'Terminal'
-    Searches GitHub for 'Terminal'
-#>
-function Search-Web(
-    # The search query to perform
-    [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
-    [string] $Query,
+# Search-Engines Data
+$Script:SearchEngines = Get-Content -Path $Script:SearchEnginesJsonFile | ConvertFrom-Json
 
-    # The search engine to use to perform the search
-    [string] $Engine = "google"
-) {
-    # Encode the query string
-    $EncodedQuery = [System.Web.HttpUtility]::UrlEncode($Query)
- 
-    # Build the search query URL
-    $Search = $SearchEngines | Where-Object { $_.shortcut -eq $Engine } | Select-Object -First 1
-    $Query = $Search.url.Replace("%s", $EncodedQuery)
- 
-    # Launch the URL using the Start-Process cmdlet (opens the URL with the default browser)
-    Start-Process $Query
+# Import Library
+Get-ChildItem -Path "$PSScriptRoot\Library" -Filter "*.ps1" | ForEach-Object {
+    . $_.FullName -Force -Verbose
 }
