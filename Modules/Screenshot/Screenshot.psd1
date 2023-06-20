6076b6746a1ec9b42002422b0ee4db5cd82b49e8
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-06-20 17:54:15 +0530

    Create `Get-ScreenshotFolder` and `Set-ScreenshotFolder`

--- Modules/Screenshot/Screenshot.psd1
+++ Modules/Screenshot/Screenshot.psd1
@@ -74,6 +74,8 @@
     FunctionsToExport = @(
         'Get-Screenshot'
         'New-Screenshot'
+        'Get-ScreenshotFolder',
+        'Set-ScreenshotFolder'
     )
 
     # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
--- Modules/Screenshot/Screenshot.psm1
+++ Modules/Screenshot/Screenshot.psm1
@@ -1,6 +1,19 @@
 # Screenshot Folder
 $Script:ScreenshotFolder = "$HOME\Pictures\Screenshots"
 
+function Get-ScreenshotFolder() {
+    return $Script:ScreenshotFolder
+}
+
+function Set-ScreenshotFolder(
+    [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+    [Alias("Folder")]
+    [ValidateScript({ Test-Path -Path $_ })]
+    [string] $Path
+) {
+    $Script:ScreenshotFolder = $Path
+}
+
 # Import Library
 Get-ChildItem -Path "$PSScriptRoot\Library" -Filter "*.ps1" | ForEach-Object {
     . $_.FullName -Force -Verbose
