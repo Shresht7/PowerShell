7a0578635c2b900f359e18de5ceb01923f8ff552
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-06-21 22:23:20 +0530

    Setup the Module with Private and Public folders

--- Modules/Search-Web/Search-Web.psm1
+++ Modules/Search-Web/Search-Web.psm1
@@ -4,7 +4,13 @@ $Script:SearchEnginesJsonFile = ".\searchEngines.json"
 # Search-Engines Data
 $Script:SearchEngines = Get-Content -Path $Script:SearchEnginesJsonFile | ConvertFrom-Json
 
-# Import Library
-Get-ChildItem -Path "$PSScriptRoot\Library" -Filter "*.ps1" | ForEach-Object {
+# Source Private Functions
+Get-ChildItem -Path "$PSScriptRoot\Private" -Filter "*.ps1" | ForEach-Object {
     . $_.FullName -Force -Verbose
+}
+
+# Source and Export Public Functions
+Get-ChildItem -Path "$PSScriptRoot\Public" -Filter "*.ps1" | ForEach-Object {
+    . $_.FullName -Force -Verbose
+    Export-ModuleMember -Function $_.BaseName
 }
