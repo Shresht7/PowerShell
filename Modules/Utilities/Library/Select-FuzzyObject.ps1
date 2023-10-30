39c252fc7aa04b3eea8dcc5d715d2c439894ae3c
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-10-30 19:33:10 +0530

    Add docstrings

--- Modules/Utilities/Library/Select-FuzzyObject.ps1
+++ Modules/Utilities/Library/Select-FuzzyObject.ps1
@@ -1,3 +1,42 @@
+<#
+.SYNOPSIS
+Selects objects using fuzzy search (fzf)
+
+.DESCRIPTION
+This cmdlet allows you to perform fuzzy searches on a collection of objects using the Fzf utility.
+
+.PARAMETER InputObject
+The input collection of objects to search. Defaults to Get-ChildItem.
+
+.PARAMETER Property
+The name of the property to use for fuzzy searching.
+
+.PARAMETER OutputScript
+A script block to transform the output.
+
+.PARAMETER Multi
+Specifies whether multiple items can be selected.
+
+.PARAMETER Preview
+A script block to customize the preview functionality.
+
+.PARAMETER FzfArgs
+Additional arguments to pass to the Fzf utility.
+
+.EXAMPLE
+Get-Process | Select-FuzzyObject -Property Name
+This example selects a process from the list of running processes based on their names.
+
+.EXAMPLE
+Get-ChildItem | Select-FuzzyObject -Property Name -Multi -Out { $_.FullName }
+This example allows selecting multiple files and returns their full paths.
+
+.NOTES
+File Name      : Select-FuzzyObject.ps1
+Author         : Shresht7
+Prerequisite   : PowerShell v3.0
+Copyright 2019 - Your Company
+#>
 function Select-FuzzyObject(
     # The Input Object
     [Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName)]
@@ -7,7 +46,7 @@ function Select-FuzzyObject(
     [string] $Property = "Name",
 
     # Transforms the resulting output using a script-block
-    [scriptblock] $Out,
+    [scriptblock] $OutputScript,
 
     # Select multiple items
     [switch] $Multi,
@@ -59,8 +98,8 @@ function Select-FuzzyObject(
         }
 
         # If the Out script is present, perform the Out script on each resulting entry and return the results
-        if ($Out) {
-            return $Result | ForEach-Object $Out
+        if ($OutputScript) {
+            return $Result | ForEach-Object $OutputScript
         }
 
         # Return the results
