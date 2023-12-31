f4b0d43dfb6ada0faf1f4ebdd614e18d76743386
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-12-31 10:45:53 +0530

    Add `ValidateScript`

--- Modules/Utilities/Library/Open-Project.ps1
+++ Modules/Utilities/Library/Open-Project.ps1
@@ -11,27 +11,26 @@
 function Open-Project(
     # The path to the project to open
     [Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName)]
+    [ValidateScript({ Test-Path -Path $_ })]
     [Alias('Name')]
     [string] $Path,
 
     # The path to the projects directory
-    [string] $ProjectDirectory = "$HOME\Projects",
-
-    # The preview command
-    [string] $PreviewCommand = 'pwsh -NoProfile -Command if (Test-Path -Path {}\README.md) { bat --style=numbers --color=always {}\README.md } else { Get-ChildItem -Path {} }'
-
-
+    [ValidateScript({ Test-Path -Path $_ })]
+    [string] $ProjectDirectory = "$HOME\Projects"
 ) {
+
     # Interactively select a project if none was provided
     if (-not $Path) {
         $Path = Get-ChildItem -Path $ProjectDirectory -Directory
-        | Invoke-Fzf -Preview $PreviewCommand -Height "100%" -PreviewWindow "right:60%"
+        | Invoke-Fzf -Preview 'pwsh -NoProfile -Command if (Test-Path -Path {}\README.md) { bat --style=numbers --color=always {}\README.md } else { Get-ChildItem -Path {} }' -Height "100%" -PreviewWindow "right:60%"
     }
 
     # Open the selected project in VS Code
     if ($Path) {
         code $Path
     }
+
 }
 
 Export-ModuleMember -Function Open-Project
