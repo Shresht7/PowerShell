db1dc1ce36f589ed7fbf879061bc7c528b1415d0
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-02-04 23:53:11 +0530

    Refactor `Show-CheatSheet.ps1` script to prompt user for a query instead of language and topic

--- Scripts/Show-CheatSheet.ps1
+++ Scripts/Show-CheatSheet.ps1
@@ -3,38 +3,24 @@
     Invoke the cheat.sh API
 .DESCRIPTION
     Invoke the cheat.sh API to get cheat sheets for various programming languages and tools.
+    The API is a simple HTTP GET request to https://cheat.sh/<query>.
+.EXAMPLE
+    Show-CheatSheet -Query "bash"
+    Shows the cheat sheet for bash
+.EXAMPLE
+    Show-CheatSheet -Query ":intro"
+    Shows the introduction to cheat.sh
 #>
 
 [CmdletBinding()]
 Param(
-    # The programming language or tool to get a cheat sheet for (e.g. python)
-    [Parameter(
-        ValueFromPipeline,
-        ValueFromPipelineByPropertyName,
-        Position = 0
-    )]
-    [string] $Language,
-
-    # The topic to get a cheat sheet for (e.g. for loop)
-    [Parameter(
-        ValueFromPipeline,
-        ValueFromPipelineByPropertyName,
-        ValueFromRemainingArguments
-    )]
-    [string] $Topic
+    [string] $Query
 )
 
-# Prompt the user for the Language and Topic parameters if they are not specified
-if (-not $Language) {
-    $Language = Read-Host -Prompt 'Language'
+# Prompt the user for the $Query, if not specified
+if (-not $Query) {
+    $Query = Read-Host -Prompt "Query"
 }
-if (-not $Topic) {
-    $Topic = Read-Host -Prompt 'Topic'
-}
-
-# Build the query if the Language and Topic parameters are specified
-$Topic = $Topic -join '+'
-$Query = "$Language/$Topic"
 
 # Invoke the cheat.sh API
 $Response = Invoke-WebRequest -Uri "https://cheat.sh/$Query" 
