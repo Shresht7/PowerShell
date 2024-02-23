cb5283cfecd14a3663e60295068c53ee149010c3
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-02-23 12:23:10 +0530

    Add configuration parameters to `Notify-InternetConnectionRestored.ps1` script

--- Scripts/Notify-InternetConnectionRestored.ps1
+++ Scripts/Notify-InternetConnectionRestored.ps1
@@ -2,16 +2,30 @@
 .SYNOPSIS
     Notify the user when the internet connection is restored
 .DESCRIPTION
-    This script continuously checks for the internet connection and notifies the user when the connection is restored
+    This script continuously checks for the internet connection
+    and notifies the user when the connection is restored
 .EXAMPLE
     .\Notify-InternetConnectionRestored.ps1
-    Continuously checks for the internet connection and notifies the user when the connection is restored
+    Continuously checks for the internet connection to "www.google.com"
+    every 5 seconds and notifies the user when the connection is restored
+.EXAMPLE
+    .\Notify-InternetConnectionRestored.ps1 -TargetName "www.example.com" -Interval 10
+    Continuously checks for the internet connection to "www.example.com"
+    every 10 seconds and notifies the user when the connection is restored
 #>
+
+param (
+    # The target website to check for internet connection (default: "www.google.com")
+    [string] $TargetName = "www.google.com",
+
+    # The interval in seconds between each connection check (default: 5)
+    [int]$Interval = 5
+)
 
 # Main loop to continuously check for the internet connection
 while ($True) {
     # Check if the internet connection is restored
-    $connection = Test-Connection -TargetName "www.google.com" -Count 1 -Quiet
+    $connection = Test-Connection -TargetName $TargetName -Count 1 -Quiet
 
     if ($connection.Status -eq 'Success') {
         # Internet connection is restored. Notify the user and exit the loop
@@ -20,7 +34,6 @@ while ($True) {
         break
     }
 
-    # Wait for 5 seconds before checking again
-    Start-Sleep -Seconds 5
+    # Wait for the specified interval before checking again
+    Start-Sleep -Seconds $Interval
 }
-
