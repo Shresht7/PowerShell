d3b6bc786ea5998ae8a1264f9054c6244adebcea
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-03-13 22:53:09 +0530

    Update documentation in `Get-Theme.ps1` for clarity and consistency

--- Modules/Theme/Library/Get-Theme.ps1
+++ Modules/Theme/Library/Get-Theme.ps1
@@ -1,24 +1,14 @@
 <#
 .SYNOPSIS
-    Get the current Windows theme (Light or Dark mode).
+    Get the current Windows Color Theme (Light or Dark Mode).
 .DESCRIPTION
-    This function retrieves the current Windows theme, which can be either
-    Light or Dark mode.
-
-.PARAMETER AsValue
-    If specified, returns the theme as a boolean value (True for Light, False for Dark).
-
-.PARAMETER AsString
-    If specified, returns the theme as a string ("Light" or "Dark").
-
+    This function retrieves the current Windows  Color Theme, which can be either Light or Dark.
 .EXAMPLE
     Get-Theme -AsValue
-    Retrieves the current Windows theme as a boolean value (True for Light, False for Dark).
-
+    Retrieves the current Windows Color Theme as a boolean value (True for Light, False for Dark).    
 .EXAMPLE
     Get-Theme -AsString
-    Retrieves the current Windows theme as a string ("Light" or "Dark").
-
+    Retrieves the current Windows Color Theme as a string ("Light" or "Dark").
 .NOTES
     Author: Shresht Srivastav
     Version: 1.0
@@ -27,8 +17,10 @@
 function Get-Theme {
     [CmdletBinding(DefaultParameterSetName = "AsString")]
     param(
+        # If specified, returns the theme as a boolean value (True for Light, False for Dark)
         [Parameter(ParameterSetName = "AsValue")]
         [switch] $AsValue,
+        # If specified, returns the theme as a string ("Light" or "Dark")
         [Parameter(ParameterSetName = "AsString")]
         [switch] $AsString
     )
@@ -39,6 +31,7 @@ function Get-Theme {
     # Get the current theme value from the registry
     $currentTheme = Get-ItemPropertyValue -Path $RegPath -Name SystemUsesLightTheme
 
+    # Return the theme based on the specified parameter set
     if ($AsValue) {
         return $currentTheme
     }
