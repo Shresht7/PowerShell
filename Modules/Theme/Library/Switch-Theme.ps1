77e80b5be86a8997264a323199e01fb07cbbb8c4
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-03-13 23:14:35 +0530

    Update Switch-Theme documentation for clarity and consistency

--- Modules/Theme/Library/Switch-Theme.ps1
+++ Modules/Theme/Library/Switch-Theme.ps1
@@ -1,12 +1,15 @@
 <#
 .SYNOPSIS
-    Switch between Light and Dark modes based on the current Windows theme.
+    Switch between Light and Dark modes based on the current Windows Color Theme.
 .DESCRIPTION
-    This function determines the current Windows theme and switches to the opposite theme.
+    This function determines the current Windows Color Theme and switches to the opposite theme.
     If the current theme is Dark, it switches to Light mode, and vice versa.
+.EXAMPLE
+    Switch-Theme
+    This will check the current Windows Color Theme and switch to the opposite theme (Light to Dark or Dark to Light).
 #>
 function Switch-Theme {
-    # Get the current Windows theme as a boolean value (True for Light, False for Dark)
+    # Get the current Windows Color Theme as a boolean value (1 for Light, 0 for Dark)
     $currentThemeValue = Get-Theme -AsValue
 
     # If the current theme is Dark, switch to Light mode
