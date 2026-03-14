23dc9bcdf8f74a98ad9162df116c806ecd50f970
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-03-14 18:13:58 +0530

    Auto update with `git fetch` and `git pull`

--- Scripts/Learn-X-in-Y-Minutes.ps1
+++ Scripts/Learn-X-in-Y-Minutes.ps1
@@ -5,19 +5,19 @@
     This script allows you to fuzzy search through a local sparse-checkout
     of the Learn X in Y Minutes documentation and display the content.
 
-    The sparse-checkout is stored in the user's home directory under "References/learnxinyminutes-docs".
+    The sparse-checkout is stored in the user's home directory under "~/References/learnxinyminutes-docs".
     If the repository is not already cloned, it will be cloned with sparse-checkout enabled to only checkout the files in the root of the repository;
     this includes all markdown files in the root directory.
 .EXAMPLE
     .\learn-x-in-y-minutes.ps1
-    This will open a fuzzy selection interface in the console where you can select a markdown file from the Learn X in Y Minutes documentation.
-    The content of the selected file will be displayed in `bat` with syntax highlighting.
+    This will open a fuzzy selection interface where you can select a markdown file from the Learn X in Y Minutes documentation.
+    The content of the selected files will be displayed in `bat` with syntax highlighting.
 .EXAMPLE
     .\learn-x-in-y-minutes.ps1 -Output Content
-    This will output the raw content of the selected markdown file in the console.
+    This will output the raw content of the selected markdown files.
 .EXAMPLE
     .\learn-x-in-y-minutes.ps1 -Output Path
-    This will output the location of the selected markdown files in the console. Read to be used in combination with other tools or scripts.
+    This will output the location of the selected markdown files. Read to be used in combination with other tools or scripts.
 .NOTES
     Required Modules:
         Requires `git` for cloning the repository and updating it. (https://git-scm.com/)
@@ -57,7 +57,11 @@ if (-not (Test-Path $ReferenceGitFolder)) {
     git clone --depth 1 --sparse https://github.com/adambard/learnxinyminutes-docs.git $ReferenceGitFolder
 }
 
-# TODO: Do a git fetch and pull to update the repository
+#  Do a git fetch and pull to update the repository
+if (-not ([string]::IsNullOrEmpty( $(git -C $ReferenceGitFolder fetch --porcelain) ))) {
+    Write-Verbose "Updating learnxinyminutes-docs repository at '$ReferenceGitFolder'"
+    git -C $ReferenceGitFolder pull --ff-only
+}
 
 # Fuzzy select a markdown file from the root of the learnxinyminutes-docs repository
 $Selection = Get-ChildItem -Path $ReferenceGitFolder -File -Filter *.md |
@@ -71,14 +75,15 @@ if ($null -eq $Selection) {
 
 switch ($Output) {
     "Content" {
-        # Output the content of the selected file to the console
-        Get-Content -Path $Selection.FullName
+        # Output the raw contents of the selected files
+        Get-Content -Path $Selection.FullName -Raw
     }
     "Pager" {
+        # Display the content of the selected file in `bat` with syntax highlighting
         bat $Selection.FullName --language $Selection.Extension.TrimStart('.')
     }
     "Path" {
-        # Output the path of the selected file to the console
-        $Selection.FullName
+        # Output the location of the selected files
+        $Selection
     }
 }
