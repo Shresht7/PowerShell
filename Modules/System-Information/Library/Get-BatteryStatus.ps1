0893dd3a71df8fee5ae9dd90027a71cb5ddcf7e8
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-03-21 16:08:25 +0530

    Add `OUTPUTS` section to `Get-BatteryStatus` documentation

--- Modules/System-Information/Library/Get-BatteryStatus.ps1
+++ Modules/System-Information/Library/Get-BatteryStatus.ps1
@@ -6,12 +6,15 @@
 .EXAMPLE
     Get-BatteryStatus
     Shows information about the battery
+.OUTPUTS
+    System.Windows.Forms.PowerStatus
 #>
 function Get-BatteryStatus() {
     Add-Type -AssemblyName System.Windows.Forms
 
     $PowerStatus = [System.Windows.Forms.SystemInformation]::PowerStatus
 
+    # Add custom properties for battery life percentage and time remaining to the existing properties of PowerStatus
     $PowerStatus | Select-Object -Property *,
     @{
         Name       = "BatteryLifePercentage"
