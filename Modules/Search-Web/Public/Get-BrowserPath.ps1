61318cfac00aa41f4367e66699012fe11cf5dcbd
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-03-21 19:52:42 +0530

    Add `Get-BrowserPath` function to retrieve browser data file paths

---
+++ Modules/Search-Web/Public/Get-BrowserPath.ps1
@@ -0,0 +1,36 @@
+<#
+.SYNOPSIS
+    Returns the path to various browser data files or directories.
+.DESCRIPTION
+    Retrieves the path for a specified browser and data type (Profile, Bookmarks, WebData).
+.PARAMETER Browser
+    The name of the browser (Edge, Chrome, etc).
+.PARAMETER Type
+    The type of path to retrieve (Profile, Bookmarks, WebData).
+.EXAMPLE
+    Get-BrowserPath -Browser Edge -Type Bookmarks
+.EXAMPLE
+    Get-BrowserPath -Browser Chrome -Type WebData
+#>
+function Get-BrowserPath {
+    param(
+        [Parameter(Mandatory)]
+        [ValidateSet("Edge", "Chrome")]
+        [string] $Browser,
+
+        [Parameter(Mandatory)]
+        [ValidateSet("Profile", "Bookmarks", "WebData")]
+        [string] $Type
+    )
+
+    $BasePath = switch ($Browser) {
+        "Edge" { "$Env:LOCALAPPDATA\Microsoft\Edge\User Data\Default" }
+        "Chrome" { "$Env:LOCALAPPDATA\Google\Chrome\User Data\Default" }
+    }
+
+    switch ($Type) {
+        "Profile" { return $BasePath }
+        "Bookmarks" { return Join-Path $BasePath "Bookmarks" }
+        "WebData" { return Join-Path $BasePath "Web Data" }
+    }
+}
