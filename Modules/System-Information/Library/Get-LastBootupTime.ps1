f730b51bea1af162805832ceb8294a04667fb740
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-03-21 16:09:59 +0530

    Use the `$ComputerName` parameter

--- Modules/System-Information/Library/Get-LastBootupTime.ps1
+++ Modules/System-Information/Library/Get-LastBootupTime.ps1
@@ -5,13 +5,16 @@
     Returns the last boot-up time for this computer
 .EXAMPLE
     Get-LastBootUpTime
+.OUTPUTS
+    PSCustomObject with properties ComputerName, LastBootUpTime, Duration, Days, Hours, Minutes, Seconds, Milliseconds
 #>
 function Get-LastBootUpTime(
     [Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName)]
     [Alias("Name", "HostName", "Computer", "SystemName")]
     [string] $ComputerName = $Env:COMPUTERNAME
 ) {
-    $LastBootUpTime = (Get-CimInstance -ClassName Win32_OperatingSystem | Select-Object -Property LastBootUpTime).LastBootUpTime
+    $LastBootUpTime = (Get-CimInstance -ClassName Win32_OperatingSystem -ComputerName $ComputerName
+        | Select-Object -Property LastBootUpTime).LastBootUpTime
     $Duration = New-TimeSpan -Start $LastBootUpTime -End (Get-Date)
     [PSCustomObject]@{
         ComputerName   = $ComputerName
