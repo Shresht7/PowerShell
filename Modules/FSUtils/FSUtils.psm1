cc35580cd72336f56b7cd2357e4dc61718afd63f
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-03-23 01:03:11 +0530

    Explicitly import functions from `FSUtils` module

--- Modules/FSUtils/FSUtils.psm1
+++ Modules/FSUtils/FSUtils.psm1
@@ -1,9 +1,9 @@
 # Import Private Functions
 Get-ChildItem -Path "$PSScriptRoot\Private" -Filter "*.ps1" -Recurse | ForEach-Object {
-    . $_.FullName -Force -Verbose
+    . $_.FullName
 }
 
 # Import Library
 Get-ChildItem -Path "$PSScriptRoot\Library" -Filter "*.ps1" -Recurse | ForEach-Object {
-    . $_.FullName -Force -Verbose
+    . $_.FullName
 }
