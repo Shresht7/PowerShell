d2a9ec8fab7ad0352f224bb48adc3ec114f30e5a
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-03-23 01:08:40 +0530

    Switch to `Select-Fuzzy` over fzf

--- Modules/FSUtils/Library/Set-LocationFuzzy.ps1
+++ Modules/FSUtils/Library/Set-LocationFuzzy.ps1
@@ -1,30 +1,26 @@
 <#
 .SYNOPSIS
-    Set the current location to a directory selected by fzf.
+    Set the current location to a directory selected by Select-Fuzzy.
 .DESCRIPTION
-    This function sets the current location to a directory selected using fzf (fuzzy finder).
+    This function sets the current location to a directory selected using Select-Fuzzy (fuzzy finder).
 .EXAMPLE
     Set-LocationFuzzy
-    Sets the current location to a directory selected using fzf.
+    Sets the current location to a directory selected using Select-Fuzzy.
+.NOTES
+    This function requires the Select-Fuzzy module to be installed.
 #>
 function Set-LocationFuzzy(
-    # The path to set as the initial query for fzf. It can be provided as a string or piped from the pipeline.
+    # The path to set as the initial query for Select-Fuzzy. It can be provided as a string or piped from the pipeline.
     [Parameter(Position = 0, ValueFromPipeline, ValueFromPipelineByPropertyName)]
     [Alias('InitialQuery')]
     [string] $Query,
 
-    # The path to perform the fzf search in
+    # The path to perform the Select-Fuzzy search in
     [ValidateScript({ Test-Path -Path $_ })]
     [string] $Path = $PWD.Path
 ) {
-    # Set Fzf Options
-    $FzfOptions = @{
-        Query   = $Query
-        Preview = "pwsh -NoProfile -Command Get-ChildItem -Path {}"
-    }
-
     # Set the current location to the selected directory
     Get-ChildItem -Path $Path -Directory
-    | Invoke-Fzf @FzfOptions 
+    | Select-Fuzzy -Query $Query -PreviewScript { Get-ChildItem -Path $_.FullName -ErrorAction SilentlyContinue }
     | Set-Location
 }
