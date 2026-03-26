9eafff0309199bd9288344c5e19641f708143dd3
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-03-26 21:20:13 +0530

    Enable `-Recurse` by default

--- Modules/FSUtils/Library/Set-LocationFuzzy.ps1
+++ Modules/FSUtils/Library/Set-LocationFuzzy.ps1
@@ -10,17 +10,18 @@
     This function requires the Select-Fuzzy module to be installed.
 #>
 function Set-LocationFuzzy(
-    # The path to set as the initial query for Select-Fuzzy. It can be provided as a string or piped from the pipeline.
-    [Parameter(Position = 0, ValueFromPipeline, ValueFromPipelineByPropertyName)]
-    [Alias('InitialQuery')]
-    [string] $Query,
-
     # The path to perform the Select-Fuzzy search in
+    [Parameter(Position = 0, ValueFromPipeline, ValueFromPipelineByPropertyName)]
     [ValidateScript({ Test-Path -Path $_ })]
-    [string] $Path = $PWD.Path
+    [string] $Path = $PWD.Path,
+
+    # The path to set as the initial query for Select-Fuzzy. It can be provided as a string or piped from the pipeline.
+    [Parameter(Position = 1, ValueFromPipelineByPropertyName)]
+    [Alias('InitialQuery')]
+    [string] $Query
 ) {
     # Set the current location to the selected directory
-    Get-ChildItem -Path $Path -Directory
+    Get-ChildItem -Path $Path -Directory -Recurse
     | Select-Fuzzy -Query $Query -PreviewScript { Get-ChildItem -Path $_.FullName -ErrorAction SilentlyContinue }
     | Set-Location
 }
