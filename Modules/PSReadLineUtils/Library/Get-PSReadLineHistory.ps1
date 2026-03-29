d74e21c5048e0669fc3b8b0b70582aeffd711673
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-03-29 09:46:11 +0530

    Add `OutputType` to `Get-PSReadLineHistory` cmdlet

--- Modules/PSReadLineUtils/Library/Get-PSReadLineHistory.ps1
+++ Modules/PSReadLineUtils/Library/Get-PSReadLineHistory.ps1
@@ -8,36 +8,39 @@
     Returns the contents of the PSReadLine history file.
 #>
 function Get-PSReadLineHistory {
-    $History = [System.Collections.ArrayList]@(
-        $last = ''
-        $lineBlock = [System.Collections.ArrayList]@()
+    [CmdletBinding()]
+    [OutputType([string[]])]
+    param ()
 
-        # Read the history file line by line
-        foreach ($line in [System.IO.File]::ReadLines((Get-PSReadLineHistoryPath))) {
+    $last = ''
+    $lineBlock = [System.Collections.ArrayList]@()
 
-            # If the line ends with a backtick, it's a continuation of the previous line
-            if ($line.EndsWith('`')) {
-                # Remove the backtick and add the line to the lineBlock variable
-                $line = $line.SubString(0, $line.Length - 1)
-                $lineBlock.Add($line) | Out-Null
-                continue
-            }
+    # Read the history file line by line
+    foreach ($line in [System.IO.File]::ReadLines((Get-PSReadLineHistoryPath))) {
 
-            # If the line does not end with a backtick, but we have an existing lineBlock,
-            # then add the line to the lineBlock and join the lines together with a newline
-            # and reset the lineBlock variable
-            if ($lineBlock) {
-                $lineBlock.Add($line) | Out-Null
-                $line = $lineBlock -join "`n"
-                $lineBlock = @()
-            }
+        # If the line ends with a backtick, it's a continuation of the previous line
+        if ($line.EndsWith('`')) {
+            # Remove the backtick and add the line to the lineBlock variable
+            $line = $line.SubString(0, $line.Length - 1)
+            $lineBlock.Add($line) | Out-Null
+            continue
+        }
+
+        # If the line does not end with a backtick, but we have an existing lineBlock,
+        # then add the line to the lineBlock and join the lines together with a newline
+        # and reset the lineBlock variable
+        if ($lineBlock) {
+            $lineBlock.Add($line) | Out-Null
+            $line = $lineBlock -join "`n"
+            $lineBlock = @()
+        }
     
-            # If the line is not the same as the last line, add it to the history
-            if ($line -ne $last) {
-                $last = $line
-                $line
-            }
-        }    
-    )
+        # If the line is not the same as the last line, add it to the history
+        if ($line -ne $last) {
+            $last = $line
+            $line
+        }
+    }    
+
     return $History
 }
