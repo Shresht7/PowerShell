e8d62e5a331c516883bfb6a196dd640ef99f25c8
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-03-29 10:10:29 +0530

    Refactor `Get-PSReadLineHistory` to improve command accumulation and completion handling

--- Modules/PSReadLineUtils/Library/Get-PSReadLineHistory.ps1
+++ Modules/PSReadLineUtils/Library/Get-PSReadLineHistory.ps1
@@ -23,35 +23,22 @@ function Get-PSReadLineHistory {
         return Get-Content -Path (Get-PSReadLineHistoryPath)
     }
 
-    $last = ''
-    $lineBlock = [System.Collections.ArrayList]@()
+    $commandBuffer = [System.Collections.ArrayList]@()
 
-    # Read the history file line by line
+    # Read the history file line-by-line...
     foreach ($line in [System.IO.File]::ReadLines((Get-PSReadLineHistoryPath))) {
+        # Add the line to the buffer
+        $commandBuffer.Add($line) | Out-Null
+        $command = $commandBuffer -join "`n"
 
-        # If the line ends with a backtick, it's a continuation of the previous line
-        if ($line.EndsWith('`')) {
-            # Remove the backtick and add the line to the lineBlock variable
-            $line = $line.SubString(0, $line.Length - 1)
-            $lineBlock.Add($line) | Out-Null
-            continue
+        # If the command is complete, then output it and clear the buffer for the next command
+        if (Test-CommandComplete $command) {
+            $commandBuffer.Clear()
+            $command
         }
 
-        # If the line does not end with a backtick, but we have an existing lineBlock,
-        # then add the line to the lineBlock and join the lines together with a newline
-        # and reset the lineBlock variable
-        if ($lineBlock) {
-            $lineBlock.Add($line) | Out-Null
-            $line = $lineBlock -join "`n"
-            $lineBlock = @()
-        }
-    
-        # If the line is not the same as the last line, add it to the history
-        if ($line -ne $last) {
-            $last = $line
-            $line
-        }
-    }    
+        # If the command is not complete, then continue accumulating
+    }
 }
 
 function Test-CommandComplete([string] $Command) {
