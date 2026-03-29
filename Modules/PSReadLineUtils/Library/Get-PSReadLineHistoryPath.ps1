1840b406c7709889be5b9ac35650d59526a12eba
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-03-29 09:39:37 +0530

    Enhance `Get-PSReadLineHistoryPath` function to support path type parameter and improve error handling

--- Modules/PSReadLineUtils/Library/Get-PSReadLineHistoryPath.ps1
+++ Modules/PSReadLineUtils/Library/Get-PSReadLineHistoryPath.ps1
@@ -7,16 +7,41 @@
     Get-PSReadLineHistoryPath
     Returns the path to the PSReadLine history file
 .EXAMPLE
+    Get-PSReadLineHistoryPath -Type Directory
+    Returns the path to the parent directory of the PSReadLine history file
+.EXAMPLE
     code (Get-PSReadLineHistoryPath)
     Opens the PSReadLine history file with VS Code
 .EXAMPLE
-    Set-Location (Split-Path (Get-PSReadLineHistoryPath))
+    Get-PSReadLineHistoryPath -Type Directory | Set-Location
     Set-Location to parent folder of the PSReadLine history file
 #>
 function Get-PSReadLineHistoryPath {
     [CmdletBinding()]
     [OutputType([string])]
-    param ()
+    param (
+        # The type of path to return. 'File' returns the full path to the history file,
+        # while 'Directory' returns the parent directory of the history file.
+        [Parameter()]
+        [ValidateSet('File', 'Directory')]
+        [string] $Type = 'File'
+    )
 
-    return (Get-PSReadLineOption).HistorySavePath
+    # Get the history path from PSReadLine options
+    $historyPath = (Get-PSReadLineOption).HistorySavePath
+
+    # Validate the history path. It should be a valid file path.
+    if (-not $historyPath -or (Test-Path $historyPath -PathType Leaf) -eq $false) {
+        throw "PSReadLine history path is not set or invalid. Please check the HistorySavePath option in PSReadLine."
+    }
+
+    # Return the appropriate path based on the specified type
+    switch ($Type) {
+        'Directory' {
+            return (Split-Path $historyPath)
+        }
+        'File' {
+            return $historyPath
+        }
+    }
 }
