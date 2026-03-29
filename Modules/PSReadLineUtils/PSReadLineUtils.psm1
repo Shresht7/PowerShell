c95b7f209e72813419fe14628ae20cf97e8f11f2
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-03-29 09:34:31 +0530

    Add check to ensure `PSReadLine` is loaded before importing the module

--- Modules/PSReadLineUtils/PSReadLineUtils.psm1
+++ Modules/PSReadLineUtils/PSReadLineUtils.psm1
@@ -2,6 +2,11 @@
 # PSReadLineHistory Utilities
 # ===========================
 
+# Ensure PSReadLine is loaded before importing this module
+if (-not (Get-Module -Name PSReadLine)) {
+    throw "PSReadLine is required but not loaded. Please import PSReadLine before importing this module."
+}
+
 # Import Library
 Get-ChildItem -Path "$PSScriptRoot\Library" -Filter "*.ps1" | ForEach-Object {
     . $_.FullName
