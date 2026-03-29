bb60cea730d8cd9f10b067cc7a811785cb7d4dff
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-03-29 18:35:59 +0530

    Improve `Connect-Module` cmdlet

--- Modules/Utilities/Library/Link/Connect-Module.ps1
+++ Modules/Utilities/Library/Link/Connect-Module.ps1
@@ -7,35 +7,31 @@
     Connect-Module -Path "MyModule.psm1"
     Links the MyModule path to the `$PSModulePath`
 #>
-function Connect-Module(
-    # The path to the module to link
-    [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
-    [Alias("Name", "Source", "SourcePath", "From")]
-    [string] $Path,
+function Connect-Module {
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        # The path to the module to link
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName, Position = 0)]
+        [Alias("Name", "FullName", "Source", "SourcePath", "From")]
+        [string] $Path,
 
-    # The destination path to link the module to (usually `$HOME\Documents\PowerShell\Modules`).
-    [Alias("DestinationPath", "Target", "TargetPath", "To")]
-    [string] $Destination
-) {
-    # Creating symbolic links requires elevated permissions
-    if (-not (Test-IsElevated)) {
-        Write-Error "Not in Administrator Mode! Elevated permissions are required to create Symbolic-Links"
-        return
-    }
+        # The destination path to link the module to (usually `$HOME\Documents\PowerShell\Modules`).
+        [Alias("DestinationPath", "Target", "TargetPath", "To")]
+        [string] $Destination
+    )
+    
+    # Determine the delimiter based on the operating system
+    $Delimiter = if ($IsWindows) { ";" } else { ":" }
 
     # If no destination is provided, use the first path in `$PSModulePath`
     if ([string]::IsNullOrWhiteSpace($Destination)) {
-        if ($PSVersionTable.OS -like "*Windows*") {
-            $Destination = $Env:PSModulePath.Split(";")[0]
-        }
-        else {
-            $Destination = $Env:PSModulePath.Split(":")[0] 
-        }
+        $Destination = $Env:PSModulePath.Split($Delimiter)[0]
     }
    
     # Create Symbolic Links
     $Module = Get-Item -Path $Path
-    New-Item -ItemType SymbolicLink -Path "$Destination\$($Module.BaseName)" -Target $Module.DirectoryName -Force
+    $Destination = Resolve-Path (Join-Path -Path $Destination -ChildPath $Module.BaseName)
+    if ($PSCmdlet.ShouldProcess("Linking $($Module.FullName) to $Destination")) {
+        New-Item -ItemType SymbolicLink -Path $Destination -Target $Module.DirectoryName -Force
+    }
 }
-
-Export-ModuleMember -Function Connect-Module
