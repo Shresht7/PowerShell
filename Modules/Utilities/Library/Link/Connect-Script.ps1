1be2a66ec129f3a41e1af1b731a332fe98372ae9
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-03-29 18:29:31 +0530

    Improve `Connect-Script` cmdlet

--- Modules/Utilities/Library/Link/Connect-Script.ps1
+++ Modules/Utilities/Library/Link/Connect-Script.ps1
@@ -2,29 +2,35 @@
 .SYNOPSIS
     Link the script to `$HOME\Scripts` folder
 .DESCRIPTION
-    Creates a symbolic link for the script in the `$HOME\Scripts` folder
+    Creates a symbolic link for the script in the `$HOME\Scripts` folder.
+    Since the script is linked to a folder in the PATH environment variable,
+    it can be run from anywhere by just typing its name.
 .EXAMPLE
     Connect-Script -Path "Get-Library.ps1"
     Links the `Get-Library.ps1` to the `$HOME\Scripts` folder
+.EXAMPLE
+    Connect-Script -Path "Get-Library.ps1" -Destination "$HOME\Documents\PowerShell\Scripts"
+    Links the `Get-Library.ps1` to the `$HOME\Documents\PowerShell\Scripts` folder
+.NOTES
+    This script requires either elevated permissions (administrator mode) or developer-mode to create symbolic links on Windows.
 #>
-function Connect-Script(
-    [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
-    [Alias("Name", "Source", "SourcePath", "From")]
-    [string] $Path,
+function Connect-Script {
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        # The path to the script to link. This can be a relative or absolute path
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName, Position = 0)]
+        [Alias("Name", "FullName", "Source", "SourcePath", "From")]
+        [string] $Path,
 
-    [Alias("DestinationPath", "Target", "TargetPath", "To")]
-    [string] $Destination = "$HOME\Scripts"
-) {
+        # The destination folder for the symbolic link. Defaults to `$HOME\Scripts`
+        [Alias("DestinationPath", "Target", "TargetPath", "To")]
+        [string] $Destination = "$HOME\Scripts"
+    )
 
-    # Creating symbolic links requires elevated permissions
-    if (-not (Test-IsElevated)) {
-        Write-Error "Not in Administrator Mode! Elevated permissions are required to create Symbolic-Links"
-        return
-    }
-
-    # Create Symbolic Links
     $Script = Get-Item -Path $Path
-    New-Item -ItemType SymbolicLink -Path "$Destination\$($Script.Name)" -Target $Script.FullName -Force
-}
+    $Destination = Resolve-Path (Join-Path -Path $Destination -ChildPath $Script.Name)
 
-Export-ModuleMember -Function Connect-Script
+    if ($PSCmdlet.ShouldProcess("Linking $($Script.FullName) to $Destination")) {
+        New-Item -ItemType SymbolicLink -Path $Destination -Target $Script.FullName -Force
+    }
+}
