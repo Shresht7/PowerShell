e9a992cbbb8660e0e465f980d9c2fd403b9cefcb
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-04-04 16:44:00 +0530

    Add `-Directory` switch to show directory

--- Modules/Utilities/Library/Find-Path.ps1
+++ Modules/Utilities/Library/Find-Path.ps1
@@ -6,13 +6,31 @@ Locates the path for the given program's executable like the Unix `which` comman
 .EXAMPLE
 Find-Path git
 Returns C:\Program Files\Git\cmd\git.exe
+.EXAMPLE
+Find-Path git -Directory
+Returns C:\Program Files\Git\cmd
+.EXAMPLE
+which node
+Returns C:\Program Files\nodejs\node.exe
+.EXAMPLE
+which -dir node
+Returns C:\Program Files\nodejs
 #>
 function Find-Path(
     [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
     [Alias("Name", "Executable", "Path")]
-    [string] $Command
+    [string] $Command,
+
+    [Alias("Dir", "Folder", "Base")]
+    [switch] $Directory
 ) {
-    Get-Command -Name $Command -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+    $FoundPath = Get-Command -Name $Command -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+
+    if ($Directory) {
+        return $FoundPath | Split-Path
+    }
+
+    $FoundPath
 }
 
 Set-Alias -Name which -Value Find-Path -Scope Global
