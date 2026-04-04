b99e36db32a1cdfa3edec3adc0f8b87aabf790ff
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-04-04 16:44:24 +0530

    Fix formatting in documentation for `Find-Path` function examples

--- Modules/Utilities/Library/Find-Path.ps1
+++ Modules/Utilities/Library/Find-Path.ps1
@@ -1,20 +1,20 @@
 <#
 .SYNOPSIS
-Find the path of the given program's executable
+    Find the path of the given program's executable
 .DESCRIPTION
-Locates the path for the given program's executable like the Unix `which` command.
+    Locates the path for the given program's executable like the Unix `which` command.
 .EXAMPLE
-Find-Path git
-Returns C:\Program Files\Git\cmd\git.exe
+    Find-Path git
+    Returns C:\Program Files\Git\cmd\git.exe
 .EXAMPLE
-Find-Path git -Directory
-Returns C:\Program Files\Git\cmd
+    Find-Path git -Directory
+    Returns C:\Program Files\Git\cmd
 .EXAMPLE
-which node
-Returns C:\Program Files\nodejs\node.exe
+    which node
+    Returns C:\Program Files\nodejs\node.exe
 .EXAMPLE
-which -dir node
-Returns C:\Program Files\nodejs
+    which -dir node
+    Returns C:\Program Files\nodejs
 #>
 function Find-Path(
     [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
